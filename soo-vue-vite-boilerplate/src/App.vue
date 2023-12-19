69959920cd4707c75fe15ff84e544c06c5409827
soo <gjstn0308@naver.com>
2023-12-20 00:02:43 +0900

    modify options api setting

--- soo-vue-vite-boilerplate/src/App.vue
+++ soo-vue-vite-boilerplate/src/App.vue
@@ -1,27 +1,17 @@
-<script setup>
+<script>
 import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <template>
-  <div class="main">
+  <div>
     <HelloWorld msg="Vuex. wtih 아아덕후" />
   </div>
 </template>
 
-<style scoped>
-.main {
-  margin: auto 40%;
-}
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
