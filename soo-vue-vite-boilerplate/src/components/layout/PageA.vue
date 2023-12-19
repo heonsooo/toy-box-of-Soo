3671c7b30a6521bad8663458a252dd99f73fccc2
soo <gjstn0308@naver.com>
2023-12-19 23:01:48 +0900

    add layout pages

---
+++ soo-vue-vite-boilerplate/src/components/layout/PageA.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="pageA-border">hi Page A</div>
+</template>
+
+<style>
+.pageA-border {
+  border: 0.5px solid #c59e9e;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
---
+++ soo-vue-vite-boilerplate/src/components/layout/PageB.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="pageB-border">hi Page B</div>
+</template>
+
+<style>
+.pageB-border {
+  border: 0.5px solid #499784;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
