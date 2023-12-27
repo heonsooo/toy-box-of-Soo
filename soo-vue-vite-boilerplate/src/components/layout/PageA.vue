7531214c6bf0aa1624dac132e286e50dd2c90e69
soo <gjstn0308@naver.com>
2023-12-27 23:24:17 +0900

    add page img

--- soo-vue-vite-boilerplate/src/components/layout/PageA.vue
+++ soo-vue-vite-boilerplate/src/components/layout/PageA.vue
@@ -3,13 +3,23 @@ export default {};
 </script>
 
 <template>
-  <div class="pageA-border">hi Page A</div>
+  <div class="pageA-border">
+    hi Page A
+    <div class="img-box"></div>
+  </div>
 </template>
 
-<style>
+<style scoped>
 .pageA-border {
   border: 0.5px solid #c59e9e;
   padding: 10px;
   margin: 10px;
+}
+
+.img-box {
+  background-image: url("../../assets/img/americano.jpg");
+  background-size: cover;
+  width: 400px;
+  height: 400px;
 }
 </style>
--- soo-vue-vite-boilerplate/src/components/layout/PageB.vue
+++ soo-vue-vite-boilerplate/src/components/layout/PageB.vue
@@ -3,13 +3,22 @@ export default {};
 </script>
 
 <template>
-  <div class="pageB-border">hi Page B</div>
+  <div class="pageB-border">
+    hi Page B
+    <div class="img-box"></div>
+  </div>
 </template>
 
-<style>
+<style scoped>
 .pageB-border {
   border: 0.5px solid #499784;
   padding: 10px;
   margin: 10px;
+}
+.img-box {
+  background-image: url("../../assets/img/latte.jpg");
+  background-size: cover;
+  width: 400px;
+  height: 400px;
 }
 </style>
