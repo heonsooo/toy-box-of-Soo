2489b377ace70504838a12f293afd35eb08d2982
soo <gjstn0308@naver.com>
2024-02-10 18:35:41 +0900

    Update Page component data

--- soo-vue-vite-boilerplate/src/components/layout/PageA.vue
+++ soo-vue-vite-boilerplate/src/components/layout/PageA.vue
@@ -1,11 +1,37 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pageTitle: "Page A",
+      instanceDataOfPageA: 72,
+    };
+  },
+  methods: {
+    onClickAddNum() {
+      this.instanceDataOfPageA++;
+    },
+    onClickMinusNum() {
+      this.instanceDataOfPageA--;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="pageA-border">
-    hi Page A
+    <h2>{{ pageTitle }}</h2>
     <div class="img-box"></div>
+    <div label="instance-data">
+      <h4>page A 인스턴스 변수</h4>
+      <ul>
+        <li>
+          별점 <span class="desc">(default : 72)</span> :
+          {{ instanceDataOfPageA }}
+        </li>
+      </ul>
+      <button class="btn" type="button" @click="onClickAddNum()">증가</button>
+      <button class="btn" type="button" @click="onClickMinusNum()">감소</button>
+    </div>
   </div>
 </template>
 
@@ -15,7 +41,13 @@ export default {};
   padding: 10px;
   margin: 10px;
 }
-
+.btn {
+  margin-left: 10px;
+}
+.desc {
+  color: grey;
+  font-size: 9pt;
+}
 .img-box {
   background-image: url("../../assets/img/americano.jpg");
   background-size: cover;
--- soo-vue-vite-boilerplate/src/components/layout/PageB.vue
+++ soo-vue-vite-boilerplate/src/components/layout/PageB.vue
@@ -1,11 +1,38 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pageTitle: "Page B",
+      instanceDataOfPageB: 35,
+    };
+  },
+  methods: {
+    onClickAddNum() {
+      this.instanceDataOfPageB++;
+    },
+    onClickMinusNum() {
+      this.instanceDataOfPageB--;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="pageB-border">
-    hi Page B
+    <h2>{{ pageTitle }}</h2>
     <div class="img-box"></div>
+    <div label="instance-data">
+      <h4>page B 인스턴스 변수</h4>
+
+      <ul>
+        <li>
+          별점<span class="desc">(default :35)</span> :
+          {{ instanceDataOfPageB }}
+        </li>
+      </ul>
+      <button class="btn" type="button" @click="onClickAddNum()">증가</button>
+      <button class="btn" type="button" @click="onClickMinusNum()">감소</button>
+    </div>
   </div>
 </template>
 
@@ -20,5 +47,13 @@ export default {};
   background-size: cover;
   width: 400px;
   height: 400px;
+}
+.desc {
+  color: grey;
+  font-size: 9pt;
+}
+
+.btn {
+  margin-left: 10px;
 }
 </style>
