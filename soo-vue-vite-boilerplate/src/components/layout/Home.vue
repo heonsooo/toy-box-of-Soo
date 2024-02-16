32785fb10018556db91e5076f2126e04e5e172bd
soo <gjstn0308@naver.com>
2024-02-16 21:57:33 +0900

    Add Home Page

---
+++ soo-vue-vite-boilerplate/src/components/layout/Home.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <section class="home-border">
+    <h2>Home</h2>
+    <p>Home Page</p>
+  </section>
+</template>
+
+<style>
+.home-border {
+  border: 0.5px solid #72abbd;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
