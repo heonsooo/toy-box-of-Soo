a1b22b0dace37d66870cf154c0c5e3098da3fc9b
soo <gjstn0308@naver.com>
2024-03-19 23:11:31 +0900

    Modify props 적용방법2 : 구조분해할당

--- soo-hello-react/src/components/Profile.jsx
+++ soo-hello-react/src/components/Profile.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function Profile(props) {
+export default function Profile({ imgUrl, name, job }) {
   return (
     <div className="profile">
       <img
         className="profile-img"
         style={{ width: "400px", height: "400px" }}
-        src={props.imgUrl}
+        src={imgUrl}
         alt="profile-pic"
       />
-      <h1>{props.name}</h1>
-      <p>{props.job}</p>
+      <h1>{name}</h1>
+      <p>{job}</p>
     </div>
   );
 }
