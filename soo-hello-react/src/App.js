64e4857444f727245d7e44f38de3e3d71f36afb0
soo <gjstn0308@naver.com>
2024-03-19 18:34:54 +0900

    apply first jsx

--- soo-hello-react/src/App.js
+++ soo-hello-react/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Fragment } from "react";
 
 function App() {
+  const myName = "Soo";
+  const friendName = "Joe";
+  const fruits = ["apple", "banana", "orange"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>
+    <Fragment>
+      <h1> Hello, React ! </h1>
+      <p> hi ! {myName}</p>
+      <p>{`hi ! ${friendName}`}</p>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={`fruit${index}`}>{fruit}</li>
+        ))}
+      </ul>
+      <img
+        style={{ width: "200px", height: "100px" }}
+        src="https://images.unsplash.com/photo-1497294815431-9365093b7331?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="img"
+      />
+    </Fragment>
+    // </>
   );
 }
 
