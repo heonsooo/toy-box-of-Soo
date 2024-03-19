66b87f50bd2f091793182377a6cfb33c2a52f091
soo <gjstn0308@naver.com>
2024-03-19 23:09:18 +0900

    Add 프로필 컴포넌트 및 props 적용

--- soo-hello-react/src/AppProfile.jsx
+++ soo-hello-react/src/AppProfile.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
-import Profile from "./components/Profile";
+import Profile from "./components/Profile.jsx";
 
 function AppProfile() {
-  return <Profile></Profile>;
+  return (
+    <>
+      <Profile
+        imgUrl="https://plus.unsplash.com/premium_photo-1665952050053-31ac47c6ff4b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y3V0ZXxlbnwwfHwwfHx8MA%3D%3D"
+        name="Kevin"
+        job="front-end Developer"
+      />
+      <Profile
+        imgUrl="https://images.unsplash.com/photo-1497752531616-c3afd9760a11?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y3V0ZXxlbnwwfHwwfHx8MA%3D%3D"
+        name="Jame"
+        job="Devops Developer"
+      />
+      <Profile
+        imgUrl="https://images.unsplash.com/photo-1533743983669-94fa5c4338ec?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGN1dGV8ZW58MHx8MHx8fDA%3D"
+        name="Poo"
+        job="Back-end Developer"
+      />
+    </>
+  );
 }
 
 export default AppProfile;
--- soo-hello-react/src/components/Profile.jsx
+++ soo-hello-react/src/components/Profile.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Profile() {
-  return <div>profile</div>;
+export default function Profile(props) {
+  return (
+    <div className="profile">
+      <img
+        className="profile-img"
+        style={{ width: "400px", height: "400px" }}
+        src={props.imgUrl}
+        alt="profile-pic"
+      />
+      <h1>{props.name}</h1>
+      <p>{props.job}</p>
+    </div>
+  );
 }
