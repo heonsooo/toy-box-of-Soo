cdd3c1b61259f868ed54b5608243e4cecd5fb541
soo <gjstn0308@naver.com>
2024-04-01 23:31:24 +0900

    Move path img files

--- soo-hello-react/src/AppXY.jsx
+++ soo-hello-react/src/AppXY.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./AppXY.css";
 export default function AppXY() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  
   const handleMouseMove = (e) => {
     setPosition({ x: e.clientX, y: e.clientY });
     console.log("x : ", e.clientX, e.clientY);
